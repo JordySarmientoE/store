628ebdff0ef95084e578cdef5a1d99f2d7bbdef8
Jordy Sarmiento Espinoza <jordysarmiento@gmail.com>
2022-03-23 23:36:11 -0500

    Adding orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,15 @@
 module.exports = {
-    "type": "postgres",
-    "host": "localhost",
-    "port": 3306,
-    "username": "test",
-    "password": "test",
-    "database": "test"
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    migrations: ['src/database/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+    entities: ['src/**/*.entity.ts'],
+    cli: {
+        migrationsDir: 'src/database/migrations',
+    },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 }
