2a3ea5e062c0a3e1f950173dc29e1f5cc5487b11
Jordy Sarmiento Espinoza <jordysarmiento@gmail.com>
2022-03-23 23:10:25 -0500

    Deleting mysql

--- src/configSchema.ts
+++ src/configSchema.ts
@@ -4,11 +4,6 @@ import * as Joi from 'joi';
 export default Joi.object({
     API_KEY: Joi.number().required(),
     DATABASE_URL: Joi.string().required(),
-    /* MYSQL_DATABASE: Joi.string().required(),
-    MYSQL_USER: Joi.string().required(),
-    MYSQL_ROOT_PASSWORD: Joi.string().required(),
-    MYSQL_PORT: Joi.number().required(),
-    MYSQL_HOST: Joi.string().required(), */
     DATABASE: Joi.string().valid('postgres','mysql').required(),
     JWT_SECRET: Joi.string().required(),
     PORT: Joi.number().required()
